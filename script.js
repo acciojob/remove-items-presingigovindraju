2dba94b484d81229ceafdbe5f55c2ecb6b00c3f2
presingigovindraju <141930032+presingigovindraju@users.noreply.github.com>
2023-10-26 16:51:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
 //your JS code here. If required.
-function deleteMe(){
-	const select = document.querySelector("select");
-	let selectValue = select.value;
-	let options = select.children;
-	for(let i = 0 ; i < options.length ;i++){
-		if(options[i].value === select.value){
-			options[i].remove();
-			break;
-		}
-	}
-}
+
